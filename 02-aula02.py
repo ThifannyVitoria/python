1373680b3e8421b77265869bd196be9be4f5538a
Thifanny <thifanny.barbosa7@gmail.com>
2024-09-13 10:22:44 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,15 @@
+# Atribuição de variáveis
+nome = "Thifanny"
+idade = "16"
+altura = "1.65"
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ", idade, "altura: ", idade ,)
+#Formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+nome = input("digite a seu nome: ")
+idade = int("digite a sua idade: ")
+altura = float(input("digite sua altura: "))
+
+print(f"nome: {nome} idade: {idade} altura {altura}")
