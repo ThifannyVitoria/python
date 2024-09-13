8a6e94afa2d8bacbd4e66c4d4396621d49e72031
Thifanny <thifanny.barbosa7@gmail.com>
2024-09-13 10:20:57 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Thifanny") # Comentário a linha de código
+print('Thif')
+print('Thifanny "Thif" Espirito')
+print("Thifanny'Thif' Espirto") 
+print("Thifanny\"Thif\"Espirito")
+
+teste = "oi"
+print(type(teste))
+teste = 16
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
